4593d9a8f49aece3a868d611c6df960e3826212c
Đinh Trọng Đạt <71766267+Dat0309@users.noreply.github.com>
2021-03-29 10:19:13 +0700

    bo sung

---
+++ Lab03_QLNXHH.sql
@@ -0,0 +1,457 @@
+﻿CREATE DATABASE Lab03_QLNXHH
+GO
+USE Lab03_QLNXHH
+GO
+CREATE TABLE HangHoa
+(MaHH CHAR(5) PRIMARY KEY,
+TenHH VARCHAR(25) NOT NULL UNIQUE,
+DVT NVARCHAR(10) NOT NULL,
+SoLuongTon TINYINT
+)
+go
+CREATE TABLE DoiTac
+(MaDT CHAR(5) PRIMARY KEY,
+TenDT NVARCHAR(30) NOT NULL UNIQUE,
+DiaChi NVARCHAR(40),
+DienThoai CHAR(10)
+)
+go
+CREATE TABLE KhaNangCC
+(MaDT CHAR(5) REFERENCES dbo.DoiTac(MaDT),
+MAHH CHAR(5) REFERENCES dbo.HangHoa(MaHH),
+PRIMARY KEY(MaDT,MAHH)
+)
+go
+CREATE TABLE HoaDon
+(SoHD CHAR(5) PRIMARY KEY,
+NgayLapHD DATETIME NOT NULL,
+MaDT CHAR(5) REFERENCES dbo.DoiTac(MaDT),
+TongTG INT
+)
+go
+CREATE TABLE CT_HoaDon
+(SoHD CHAR(5) REFERENCES dbo.HoaDon(SoHD),
+MaHH CHAR(5) REFERENCES dbo.HangHoa(MaHH),
+PRIMARY KEY(SoHD,MaHH)
+)
+ALTER TABLE dbo.CT_HoaDon
+ADD DonGia INT NOT NULL,
+	SoLuong TINYINT NOT NULL
+
+--Nhap Hàng hóa
+INSERT INTO dbo.HangHoa
+VALUES
+(   'CPU01',  -- MaHH - char(5)
+    'CPU INTEL,CELERON 600 BOX',  -- TenHH - varchar(25)
+    N'CÁI', -- DVT - nvarchar(10)
+    5    -- SoLuongTon - tinyint
+    )
+INSERT INTO dbo.HangHoa
+VALUES
+(   'CPU02',  -- MaHH - char(5)
+    'CPU INTEL,PIII 700',  -- TenHH - varchar(25)
+    N'CÁI', -- DVT - nvarchar(10)
+    10    -- SoLuongTon - tinyint
+    )
+INSERT INTO dbo.HangHoa
+VALUES
+(   'CPU03',  -- MaHH - char(5)
+    'CPU AMD K7 ATHL,ON 600',  -- TenHH - varchar(25)
+    N'CÁI', -- DVT - nvarchar(10)
+    8    -- SoLuongTon - tinyint
+    )
+INSERT INTO dbo.HangHoa
+VALUES
+(   'HDD01',  -- MaHH - char(5)
+    'HDD 10.2 GB QUANTUM',  -- TenHH - varchar(25)
+    N'CÁI', -- DVT - nvarchar(10)
+    10    -- SoLuongTon - tinyint
+    )
+INSERT INTO dbo.HangHoa
+VALUES
+(   'HDD02',  -- MaHH - char(5)
+    'HDD',  -- TenHH - varchar(25)
+    N'CÁI', -- DVT - nvarchar(10)
+    15    -- SoLuongTon - tinyint
+    )
+INSERT INTO dbo.HangHoa
+VALUES
+(   'HDD03',  -- MaHH - char(5)
+    'HDD 20 GB QUANTUM',  -- TenHH - varchar(25)
+    N'CÁI', -- DVT - nvarchar(10)
+    6    -- SoLuongTon - tinyint
+    )
+INSERT INTO dbo.HangHoa
+VALUES
+(   'KB01',  -- MaHH - char(5)
+    'KB GENIUS',  -- TenHH - varchar(25)
+    N'CÁI', -- DVT - nvarchar(10)
+    12    -- SoLuongTon - tinyint
+    )
+INSERT INTO dbo.HangHoa
+VALUES
+(   'KB02',  -- MaHH - char(5)
+    'KB MITSUMIMI',  -- TenHH - varchar(25)
+    N'CÁI', -- DVT - nvarchar(10)
+    5    -- SoLuongTon - tinyint
+    )
+INSERT INTO dbo.HangHoa
+VALUES
+(   'MB01',  -- MaHH - char(5)
+    'GIGABYTE CHIPSET INTEL',  -- TenHH - varchar(25)
+    N'CÁI', -- DVT - nvarchar(10)
+    10    -- SoLuongTon - tinyint
+    )
+INSERT INTO dbo.HangHoa
+VALUES
+(   'MB02',  -- MaHH - char(5)
+    'ACOPR BX CHIPSET VIA',  -- TenHH - varchar(25)
+    N'CÁI', -- DVT - nvarchar(10)
+    10    -- SoLuongTon - tinyint
+    )
+INSERT INTO dbo.HangHoa
+VALUES
+(   'MB03',  -- MaHH - char(5)
+    'INTEL PHI CHIPSET INTEL',  -- TenHH - varchar(25)
+    N'CÁI', -- DVT - nvarchar(10)
+    10    -- SoLuongTon - tinyint
+    )
+INSERT INTO dbo.HangHoa
+VALUES
+(   'MB04',  -- MaHH - char(5)
+    'ECS CHIPSET SIS',  -- TenHH - varchar(25)
+    N'CÁI', -- DVT - nvarchar(10)
+    10    -- SoLuongTon - tinyint
+    )
+INSERT INTO dbo.HangHoa
+VALUES
+(   'MB05',  -- MaHH - char(5)
+    'ECS CHIPSET VIA',  -- TenHH - varchar(25)
+    N'CÁI', -- DVT - nvarchar(10)
+    10    -- SoLuongTon - tinyint
+    )
+INSERT INTO dbo.HangHoa
+VALUES
+(   'MNT01',  -- MaHH - char(5)
+    'SAMSUNG 14"SYNCMASTER',  -- TenHH - varchar(25)
+    N'CÁI', -- DVT - nvarchar(10)
+    5    -- SoLuongTon - tinyint
+    )
+INSERT INTO dbo.HangHoa
+VALUES
+(   'MNT02',  -- MaHH - char(5)
+    'LG14"',  -- TenHH - varchar(25)
+    N'CÁI', -- DVT - nvarchar(10)
+    5    -- SoLuongTon - tinyint
+    )
+INSERT INTO dbo.HangHoa
+VALUES
+(   'MNT03',  -- MaHH - char(5)
+    'ACER 14"',  -- TenHH - varchar(25)
+    N'CÁI', -- DVT - nvarchar(10)
+    8    -- SoLuongTon - tinyint
+    )
+INSERT INTO dbo.HangHoa
+VALUES
+(   'MNT04',  -- MaHH - char(5)
+    'PHILIPS 14"',  -- TenHH - varchar(25)
+    N'CÁI', -- DVT - nvarchar(10)
+    6    -- SoLuongTon - tinyint
+    )
+INSERT INTO dbo.HangHoa
+VALUES
+(   'MNT05',  -- MaHH - char(5)
+    'VIEWSONIC 14"',  -- TenHH - varchar(25)
+    N'CÁI', -- DVT - nvarchar(10)
+    7    -- SoLuongTon - tinyint
+    )
+
+--Nhập đối tác
+INSERT INTO dbo.DoiTac
+VALUES
+(   'CC001',  -- MaDT - char(5)
+    N'Cty TNC', -- TenDT - nvarchar(30)
+    N'176 BTX Q1 - TP.HCM', -- DiaChi - nvarchar(40)
+    '08.8250259'   -- DienThoai - char(10)
+    )
+INSERT INTO dbo.DoiTac
+VALUES
+(   'CC002',  -- MaDT - char(5)
+    N'Cty Hoàng Long', -- TenDT - nvarchar(30)
+    N'15A TTT Q1 - TP.HCM', -- DiaChi - nvarchar(40)
+    '08.0250898'   -- DienThoai - char(10)
+    )
+INSERT INTO dbo.DoiTac
+VALUES
+(   'CC003',  -- MaDT - char(5)
+    N'Cty Hợp Nhất', -- TenDT - nvarchar(30)
+    N'152 BTX Q1 - TP.HCM', -- DiaChi - nvarchar(40)
+    '08.8252376'   -- DienThoai - char(10)
+    )
+INSERT INTO dbo.DoiTac
+VALUES
+(   'K0001',  -- MaDT - char(5)
+    N'Nguyễn Minh Hải', -- TenDT - nvarchar(30)
+    N'91 Nguyễn Văn Trỗi - TP.Đà Lạt', -- DiaChi - nvarchar(40)
+    '063.831129'   -- DienThoai - char(10)
+    )
+INSERT INTO dbo.DoiTac
+VALUES
+(   'K0002',  -- MaDT - char(5)
+    N'Như Quỳnh', -- TenDT - nvarchar(30)
+    N'21 Điện Biên Phủ N.Trang', -- DiaChi - nvarchar(40)
+    '058590270'   -- DienThoai - char(10)
+    )
+INSERT INTO dbo.DoiTac
+VALUES
+(   'K0003',  -- MaDT - char(5)
+    N'Trần Nhật Dật', -- TenDT - nvarchar(30)
+    N'Lê Lợi - TP.Huế', -- DiaChi - nvarchar(40)
+    '054.848376'   -- DienThoai - char(10)
+    )
+INSERT INTO dbo.DoiTac
+VALUES
+(   'K0004',  -- MaDT - char(5)
+    N'Phan Nguyễn Hùng Anh', -- TenDT - nvarchar(30)
+    N'11 Nam Kỳ Khởi Nghĩa - TP.Đà Lạt', -- DiaChi - nvarchar(40)
+    '063.823409'   -- DienThoai - char(10)
+    )
+
+--Nhập Hóa đơn
+INSERT INTO dbo.HoaDon
+VALUES
+(   'N0001',        -- SoHD - char(5)
+    '01/25/2006', -- NgayLapHD - datetime
+    'CC001',        -- MaDT - char(5)
+     NULL         -- TongTG - int
+    )
+INSERT INTO dbo.HoaDon
+VALUES
+(   'N0002',        -- SoHD - char(5)
+    '05/01/2006', -- NgayLapHD - datetime
+    'CC002',        -- MaDT - char(5)
+    NULL          -- TongTG - int
+    )
+INSERT INTO dbo.HoaDon
+VALUES
+(   'X0001',        -- SoHD - char(5)
+    '05/12/2006', -- NgayLapHD - datetime
+    'K0001',        -- MaDT - char(5)
+    NULL          -- TongTG - int
+    )
+INSERT INTO dbo.HoaDon
+VALUES
+(   'X0002',        -- SoHD - char(5)
+    '06/16/2006', -- NgayLapHD - datetime
+    'K0002',        -- MaDT - char(5)
+    NULL          -- TongTG - int
+    )
+INSERT INTO dbo.HoaDon
+VALUES
+(   'X0003',        -- SoHD - char(5)
+    '04/20/2006', -- NgayLapHD - datetime
+    'K0001',        -- MaDT - char(5)
+    NULL          -- TongTG - int
+    )
+
+--Nhập khả năng cung cấp
+INSERT INTO dbo.KhaNangCC
+VALUES
+(   'CC001', -- MaDT - char(5)
+    'CPU01'  -- MAHH - char(5)
+    )
+INSERT INTO dbo.KhaNangCC
+VALUES
+(   'CC001', -- MaDT - char(5)
+    'HDD03'  -- MAHH - char(5)
+    )
+INSERT INTO dbo.KhaNangCC
+VALUES
+(   'CC001', -- MaDT - char(5)
+    'KB01'  -- MAHH - char(5)
+    )
+INSERT INTO dbo.KhaNangCC
+VALUES
+(   'CC001', -- MaDT - char(5)
+    'MB02'  -- MAHH - char(5)
+    )
+INSERT INTO dbo.KhaNangCC
+VALUES
+(   'CC001', -- MaDT - char(5)
+    'MB04'  -- MAHH - char(5)
+    )
+INSERT INTO dbo.KhaNangCC
+VALUES
+(   'CC001', -- MaDT - char(5)
+    'MNT01'  -- MAHH - char(5)
+    )
+INSERT INTO dbo.KhaNangCC
+VALUES
+(   'CC002', -- MaDT - char(5)
+    'CPU01'  -- MAHH - char(5)
+    )
+INSERT INTO dbo.KhaNangCC
+VALUES
+(   'CC002', -- MaDT - char(5)
+    'CPU02'  -- MAHH - char(5)
+    )
+INSERT INTO dbo.KhaNangCC
+VALUES
+(   'CC002', -- MaDT - char(5)
+    'CPU03'  -- MAHH - char(5)
+    )
+INSERT INTO dbo.KhaNangCC
+VALUES
+(   'CC002', -- MaDT - char(5)
+    'KB02'  -- MAHH - char(5)
+    )
+INSERT INTO dbo.KhaNangCC
+VALUES
+(   'CC002', -- MaDT - char(5)
+    'MB01'  -- MAHH - char(5)
+    )
+INSERT INTO dbo.KhaNangCC
+VALUES
+(   'CC002', -- MaDT - char(5)
+    'MB05'  -- MAHH - char(5)
+    )
+INSERT INTO dbo.KhaNangCC
+VALUES
+(   'CC002', -- MaDT - char(5)
+    'MNT03'  -- MAHH - char(5)
+    )
+INSERT INTO dbo.KhaNangCC
+VALUES
+(   'CC003', -- MaDT - char(5)
+    'HDD01'  -- MAHH - char(5)
+    )
+INSERT INTO dbo.KhaNangCC
+VALUES
+(   'CC003', -- MaDT - char(5)
+    'HDD02'  -- MAHH - char(5)
+    )
+INSERT INTO dbo.KhaNangCC
+VALUES
+(   'CC003', -- MaDT - char(5)
+    'HDD03'  -- MAHH - char(5)
+    )
+INSERT INTO dbo.KhaNangCC
+VALUES
+(   'CC003', -- MaDT - char(5)
+    'MB03'  -- MAHH - char(5)
+    )
+
+--Nhập CT Hóa đơn
+INSERT INTO dbo.CT_HoaDon
+VALUES
+(   'N0001', -- SoHD - char(5)
+    'CPU01',  -- MaHH - char(5)
+	 63,
+	 10
+    )
+INSERT INTO dbo.CT_HoaDon
+VALUES
+(   'N0001', -- SoHD - char(5)
+    'HDD03',  -- MaHH - char(5)
+	 97,
+	 7
+    )
+INSERT INTO dbo.CT_HoaDon
+VALUES
+(   'N0001', -- SoHD - char(5)
+    'KB01',  -- MaHH - char(5)
+	 3,
+	 5
+    )
+INSERT INTO dbo.CT_HoaDon
+VALUES
+(   'N0001', -- SoHD - char(5)
+    'MB02',  -- MaHH - char(5)
+	 57,
+	 5
+    )
+INSERT INTO dbo.CT_HoaDon
+VALUES
+(   'N0001', -- SoHD - char(5)
+    'MNT01',  -- MaHH - char(5)
+	 112,
+	 3
+    )
+INSERT INTO dbo.CT_HoaDon
+VALUES
+(   'N0002', -- SoHD - char(5)
+    'CPU02',  -- MaHH - char(5)
+	 115,
+	 3
+    )
+INSERT INTO dbo.CT_HoaDon
+VALUES
+(   'N0002', -- SoHD - char(5)
+    'KB02',  -- MaHH - char(5)
+	 5,
+	 7
+    )
+INSERT INTO dbo.CT_HoaDon
+VALUES
+(   'N0002', -- SoHD - char(5)
+    'MNT03',  -- MaHH - char(5)
+	 111,
+	 5
+    )
+INSERT INTO dbo.CT_HoaDon
+VALUES
+(   'X0001', -- SoHD - char(5)
+    'CPU01',  -- MaHH - char(5)
+	 67,
+	 2
+    )
+INSERT INTO dbo.CT_HoaDon
+VALUES
+(   'X0001', -- SoHD - char(5)
+    'KB01',  -- MaHH - char(5)
+	 5,
+	 2
+    )
+INSERT INTO dbo.CT_HoaDon
+VALUES
+(   'X0001', -- SoHD - char(5)
+    'MB02',  -- MaHH - char(5)
+	 62,
+	 1
+    )
+INSERT INTO dbo.CT_HoaDon
+VALUES
+(   'X0002', -- SoHD - char(5)
+    'CPU01',  -- MaHH - char(5)
+	 67,
+	 1
+    )
+INSERT INTO dbo.CT_HoaDon
+VALUES
+(   'X0002', -- SoHD - char(5)
+    'KB02',  -- MaHH - char(5)
+	 7,
+	 3
+    )
+INSERT INTO dbo.CT_HoaDon
+VALUES
+(   'X0002', -- SoHD - char(5)
+    'MNT01',  -- MaHH - char(5)
+	 115,
+	 2
+    )
+INSERT INTO dbo.CT_HoaDon
+VALUES
+(   'X0003', -- SoHD - char(5)
+    'CPU01',  -- MaHH - char(5)
+	 67,
+	 1
+    )
+INSERT INTO dbo.CT_HoaDon
+VALUES
+(   'X0003', -- SoHD - char(5)
+    'MNT03',  -- MaHH - char(5)
+	 115,
+	 2
+    )
+
---
+++ Lab04_QLDB.sql
@@ -0,0 +1,198 @@
+﻿CREATE DATABASE Lab04_QLDB
+GO
+USE Lab04_QLDB
+GO
+ 
+ CREATE TABLE BAO_TCHI
+ (MaBaoTC CHAR(4) PRIMARY KEY,
+ Ten NVARCHAR(20) NOT NULL,
+ DinhKy NVARCHAR(20),
+ SoLuong INT,
+ GiaBan int 
+ )
+ go
+ CREATE TABLE PHATHANH
+ (MaBaoTC CHAR(4) REFERENCES dbo.Bao_TChi(MaBaoTC),
+ SoBaoTC INT,
+ NgayPH DATETIME,
+ PRIMARY KEY(MaBaoTC,SoBaoTC)
+ )
+ go
+ CREATE TABLE KHACHHANG
+ (MaKH CHAR(5) PRIMARY KEY,
+ TenKH NVARCHAR(10) NOT NULL,
+ DiaChi VARCHAR(10)
+ )
+ go
+ CREATE TABLE DATBAO
+ (MaKH CHAR(5) REFERENCES dbo.KHACHHANG(MaKH),
+ MaBaoTC CHAR(4) REFERENCES dbo.BAO_TCHI(MaBaoTC),
+ SLMua INT NOT NULL,
+ NgayDM DATETIME
+ )
+ GO
+ --------------------------------
+INSERT INTO dbo.BAO_TCHI
+VALUES
+ (   'TT01',  -- MaBaoTC - char(4)
+     N'Tuổi trẻ', -- Ten - nvarchar(20)
+     N'Nhật báo', -- DinhKy - nvarchar(20)
+     1000,   -- SoLuong - int
+     1500    -- GiaBan - int
+     )
+INSERT INTO dbo.BAO_TCHI
+VALUES
+ (   'KT01',  -- MaBaoTC - char(4)
+     N'Kiến thức ngày nay', -- Ten - nvarchar(20)
+     N'Bán nguyệt san', -- DinhKy - nvarchar(20)
+     3000,   -- SoLuong - int
+     6000    -- GiaBan - int
+     )
+INSERT INTO dbo.BAO_TCHI
+VALUES
+ (   'TN01',  -- MaBaoTC - char(4)
+     N'Thanh niên', -- Ten - nvarchar(20)
+     N'Nhật báo', -- DinhKy - nvarchar(20)
+     1000,   -- SoLuong - int
+     2000    -- GiaBan - int
+     )
+INSERT INTO dbo.BAO_TCHI
+VALUES
+ (   'PN01',  -- MaBaoTC - char(4)
+     N'Phụ nữ', -- Ten - nvarchar(20)
+     N'Tuần báo', -- DinhKy - nvarchar(20)
+     2000,   -- SoLuong - int
+     4000    -- GiaBan - int
+     )
+INSERT INTO dbo.BAO_TCHI
+VALUES
+ (   'PN02',  -- MaBaoTC - char(4)
+     N'Phụ nữ', -- Ten - nvarchar(20)
+     N'Nhật báo', -- DinhKy - nvarchar(20)
+     1000,   -- SoLuong - int
+     2000    -- GiaBan - int
+     )
+
+INSERT INTO dbo.PHATHANH
+VALUES
+(   'TT01',       -- MaBaoTC - char(4)
+    123,        -- SoBaoTC - tinyint
+    '12/15/2005' -- NgayPH - datetime
+    )
+INSERT INTO dbo.PHATHANH
+VALUES
+(   'KT01',       -- MaBaoTC - char(4)
+    70,       -- SoBaoTC - tinyint
+    '12/15/2005' -- NgayPH - datetime
+    )
+INSERT INTO dbo.PHATHANH
+VALUES
+(   'TT01',       -- MaBaoTC - char(4)
+    124,        -- SoBaoTC - tinyint
+    '12/16/2005' -- NgayPH - datetime
+    )
+INSERT INTO dbo.PHATHANH
+VALUES
+(   'TN01',       -- MaBaoTC - char(4)
+    256,        -- SoBaoTC - tinyint
+    '12/17/2005' -- NgayPH - datetime
+    )
+INSERT INTO dbo.PHATHANH
+VALUES
+(   'PN01',       -- MaBaoTC - char(4)
+    45,        -- SoBaoTC - tinyint
+    '12/23/2005' -- NgayPH - datetime
+    )
+INSERT INTO dbo.PHATHANH
+VALUES
+(   'PN02',       -- MaBaoTC - char(4)
+    111,        -- SoBaoTC - tinyint
+    '12/18/2005' -- NgayPH - datetime
+    )
+INSERT INTO dbo.PHATHANH
+VALUES
+(   'PN02',       -- MaBaoTC - char(4)
+    112,        -- SoBaoTC - tinyint
+    '12/19/2005' -- NgayPH - datetime
+    )
+INSERT INTO dbo.PHATHANH
+VALUES
+(   'TT01',       -- MaBaoTC - char(4)
+    125,        -- SoBaoTC - tinyint
+    '12/17/2005' -- NgayPH - datetime
+    )
+INSERT INTO dbo.PHATHANH
+VALUES
+(   'PN01',       -- MaBaoTC - char(4)
+    46,        -- SoBaoTC - tinyint
+    '12/30/2005' -- NgayPH - datetime
+    )
+
+INSERT INTO dbo.KHACHHANG
+VALUES
+(   'KH01',  -- MaKH - char(5)
+    N'LAN', -- TenKH - nvarchar(10)
+    '2 NCT'   -- DiaChi - varchar(10)
+    )
+INSERT INTO dbo.KHACHHANG
+VALUES
+(   'KH02',  -- MaKH - char(5)
+    N'NAM', -- TenKH - nvarchar(10)
+    '32 THĐ'   -- DiaChi - varchar(10)
+    )
+INSERT INTO dbo.KHACHHANG
+VALUES
+(   'KH03',  -- MaKH - char(5)
+    N'NGỌC', -- TenKH - nvarchar(10)
+    '16 LHP'   -- DiaChi - varchar(10)
+    )
+
+INSERT INTO dbo.DATBAO
+VALUES
+(   'KH01',       -- MaKH - char(5)
+    'TT01',       -- MaBaoTC - char(4)
+    100,        -- SLMua - int
+    '01/12/2000' -- NgayDM - datetime
+    )
+INSERT INTO dbo.DATBAO
+VALUES
+(   'KH02',       -- MaKH - char(5)
+    'TN01',       -- MaBaoTC - char(4)
+    150,        -- SLMua - int
+    '05/01/2001' -- NgayDM - datetime
+    )
+INSERT INTO dbo.DATBAO
+VALUES
+(   'KH01',       -- MaKH - char(5)
+    'PN01',       -- MaBaoTC - char(4)
+    200,        -- SLMua - int
+    '06/25/2001' -- NgayDM - datetime
+    )
+INSERT INTO dbo.DATBAO
+VALUES
+(   'KH03',       -- MaKH - char(5)
+    'KT01',       -- MaBaoTC - char(4)
+    50,        -- SLMua - int
+    '03/17/2002' -- NgayDM - datetime
+    )
+INSERT INTO dbo.DATBAO
+VALUES
+(   'KH03',       -- MaKH - char(5)
+    'PN02',       -- MaBaoTC - char(4)
+    200,        -- SLMua - int
+    '08/26/2003' -- NgayDM - datetime
+    )
+INSERT INTO dbo.DATBAO
+VALUES
+(   'KH02',       -- MaKH - char(5)
+    'TT01',       -- MaBaoTC - char(4)
+    250,        -- SLMua - int
+    '01/15/2004' -- NgayDM - datetime
+    )
+INSERT INTO dbo.DATBAO
+VALUES
+(   'KH01',       -- MaKH - char(5)
+    'KT01',       -- MaBaoTC - char(4)
+    300,        -- SLMua - int
+    '10/14/2004' -- NgayDM - datetime
+    )
