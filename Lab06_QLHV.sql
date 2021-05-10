6d42007e5c5beaeca5c2906c92b4c53a8687152b
Đinh Trọng Đạt <71766267+Dat0309@users.noreply.github.com>
2021-05-10 10:50:08 +0700

    Bổ sung lab06,Truy vấn, Ràng buộc, Hàm, Thủ tục

--- Lab06_QLHV.sql
+++ Lab06_QLHV.sql
@@ -36,9 +36,10 @@ MSHV CHAR(10) PRIMARY KEY,
 Ho NVARCHAR(20),
 Ten NVARCHAR(10),
 NgaySinh DATETIME,
-Phai CHAR(3),
+Phai nvarCHAR(3),
 MaLop CHAR(4) REFERENCES dbo.Lop(MaLop)
 )
+
 GO
 CREATE TABLE HocPhi
 (SoBL CHAR(4) PRIMARY KEY,
@@ -50,97 +51,7 @@ NguoiThu NVARCHAR(10) NOT NULL
 )
 GO
 
---Chèn dữ liệu vào bảng
-INSERT dbo.CaHoc
-(
-    Ca,
-    GioBatDau,
-    GioKetThuc
-)
-VALUES
-(   1,         -- Ca - tinyint
-    '7:30', -- GioBatDau - datetime
-    '10:45'  -- GioKetThuc - datetime
-    )
 
-SELECT * FROM dbo.CaHoc
-
-INSERT dbo.Lop
-	(
-	    MaLop,
-	    TenLop,
-	    NgayKG,
-	    HocPhi,
-	    Ca,
-	    SoTiet,
-	    SoHV,
-	    MSGV
-	)
-	VALUES
-	(   'E114',        -- MaLop - char(4)
-	    'Excel 3-5-7',        -- TenLop - varchar(15)
-	    '01/02/2008', -- NgayKG - datetime
-	    120000,         -- HocPhi - int
-	    1,         -- Ca - tinyint
-	    45,         -- SoTiet - tinyint
-	    3,         -- SoHV - tinyint
-	    'G003'         -- MSGV - char(4)
-	    )
-DELETE FROM dbo.Lop WHERE MaLop='E114'
-SELECT * FROM dbo.Lop
-
---Chèn vào giáo viên
-INSERT dbo.GiaoVien
-		(
-		    MSGV,
-		    HoGV,
-		    TenGV,
-		    DienThoai
-		)
-		VALUES
-		(   'G003',  -- MSGV - char(4)
-		    N'Trần Minh', -- HoGV - nvarchar(20)
-		    N'Hùng', -- TenGV - nvarchar(10)
-		    '823456'   -- DienThoai - varchar(6)
-		    )
---Chèn học viên
-INSERT dbo.HocVien
-		(
-		    MSHV,
-		    Ho,
-		    Ten,
-		    NgaySinh,
-		    Phai,
-		    MaLop
-		)
-		VALUES
-		(   'W12301',        -- MSHV - char(10)
-		    N'Nguyễn Ngọc',       -- Ho - nvarchar(20)
-		    N'Tuyết',       -- Ten - nvarchar(10)
-		    '05/12/1996', -- NgaySinh - datetime
-		    'Nữ',        -- Phai - char(3)
-		    'W123'         -- MaLop - char(4)
-		    )
---Chèn vào Học phí
-INSERT dbo.HocPhi
-(
-    SoBL,
-    MSHV,
-    NgayThu,
-    SoTien,
-    NoiDung,
-    NguoiThu
-)
-VALUES
-(   '0001',        -- SoBL - char(4)
-    'E11401',        -- MSHV - char(10)
-    '01/02/2008', -- NgayThu - datetime
-    120000,         -- SoTien - int
-    'HP Excel 3-5-7',        -- NoiDung - varchar(20)
-    N'Vân'        -- NguoiThu - nvarchar(10)
-    )
-
-	--DELETE FROM dbo.HocPhi WHERE MSHV='E11401'
 --Cài đặt ràng buộc toàn vẹn
 --a.giờ kết thúc của một ca học không được trước giờ bắt đầu ca học đó (giờ bắt đầu - giờ kết thúc <0)
 CREATE TRIGGER UTG_CaHoc
@@ -165,10 +76,11 @@ CREATE TRIGGER UTG_siSo
 ON dbo.Lop
 FOR INSERT 
 AS
+IF UPDATE(MaLop) Or UPDATE(SoHV)
 BEGIN
     DECLARE @Count INT=0;
 	SELECT @Count = COUNT(*) FROM Inserted
-	WHERE Inserted.SoHV>30 AND  Inserted.SoHV>(SELECT COUNT(MaLop)
+	WHERE Inserted.SoHV>30 AND  Inserted.SoHV<>(SELECT COUNT(MSHV)
 												FROM dbo.HocVien
 												WHERE MaLop=Inserted.MaLop)
 
@@ -201,3 +113,264 @@ BEGIN
 END
 
 --DROP TRIGGER UTG_hocPhi
+
+--4.Tạo các hàm thủ tục sau:
+--a.Thêm dữ liệu vào các bảng
+CREATE PROC usp_ThemCaHoc @ca tinyint, @giobd TIME, @giokt TIME 
+AS
+	IF EXISTS(	SELECT * FROM dbo.CaHoc
+				WHERE Ca=@ca)
+				PRINT N'Đã có ca học trong CSDL'
+	ELSE 
+		BEGIN
+		    INSERT INTO dbo.CaHoc
+		    VALUES
+		    (   @ca,          -- Ca - tinyint
+		        @giobd, -- GioBatDau - time(7)
+		        @giokt  -- GioKetThuc - time(7)
+		        )
+			PRINT N'Thêm ca thành công!'
+		END
+---------------
+EXEC usp_ThemCaHoc 1,'7:30','10:45'
+---------------------------
+
+CREATE PROC usp_ThemGiaoVien @msgv CHAR(4),@hogv nvarchar(20),@ten nvarchar(10),@dt varchar(6)
+AS
+	IF EXISTS (	SELECT * FROM dbo.GiaoVien WHERE MSGV=@msgv)
+		PRINT N'Đã có giáo viên trong CSDL'
+	ELSE
+		BEGIN
+		    INSERT INTO dbo.GiaoVien
+		    VALUES
+		    (   @msgv,  -- MSGV - char(4)
+		        @hogv, -- HoGV - nvarchar(20)
+		        @ten, -- TenGV - nvarchar(10)
+		        @dt -- DienThoai - varchar(6)
+		        )
+				PRINT N'Thêm giáo viên thành công'
+		END
+
+------------------------------------
+exec usp_ThemGiaoVien 'G001',N'Lê Hoàng',N'Anh', '858936'
+exec usp_ThemGiaoVien 'G002',N'Nguyễn Ngọc',N'Lan', '845623'
+exec usp_ThemGiaoVien 'G003',N'Trần Minh',N'Hùng', '823456'
+exec usp_ThemGiaoVien 'G004',N'Võ Thanh',N'Trung', '841256'
+
+------------------------------------
+CREATE PROC usp_ThemLop @malop CHAR(4),@tenlop varchar(15),@ngaykg datetime,@hocphi int,@ca tinyint,@sotiet tinyint,@sohv tinyint,@magv CHAR(4)
+AS
+	IF EXISTS(	SELECT * FROM dbo.CaHoc WHERE Ca=@ca) AND EXISTS(	SELECT * FROM dbo.GiaoVien WHERE MSGV=@magv)
+		BEGIN
+		    IF EXISTS (	SELECT * FROM dbo.Lop WHERE MaLop=@malop)
+				PRINT N'Đã có lớp trong CSDL'
+			ELSE
+				BEGIN
+				    INSERT INTO dbo.Lop
+				    VALUES
+				    (   @malop,        -- MaLop - char(4)
+				        @tenlop,        -- TenLop - varchar(15)
+				        @ngaykg, -- NgayKG - datetime
+				        @hocphi,         -- HocPhi - int
+				        @ca,         -- Ca - tinyint
+				        @sotiet,         -- SoTiet - tinyint
+				        @sohv,         -- SoHV - tinyint
+				        @magv         -- MSGV - char(4)
+				        )
+				END
+		END
+	ELSE
+		BEGIN
+		    IF NOT EXISTS (SELECT * FROM dbo.CaHoc WHERE Ca=@ca)
+				PRINT N'Không tồn tại ca học trong CSDL'
+			ELSE
+				PRINT N'Không tồn tại giáo viên trong CSDL'
+		END
+
+------------------------------------------
+set dateformat dmy
+go
+exec usp_ThemLop 'A075',N'Access 2-4-6','18/12/2008', 150000,3,60,3,'G003'
+exec usp_ThemLop 'E114',N'Excel 3-5-7','02/01/2008', 120000,1,45,3,'G003'
+exec usp_ThemLop 'A115',N'Excel 2-4-6','22/01/2008', 120000,3,45,0,'G001'
+exec usp_ThemLop 'W123',N'Word 2-4-6','18/02/2008', 100000,3,30,1,'G001'
+exec usp_ThemLop 'W124',N'Word 3-5-7','01/03/2008', 100000,1,30,0,'G002'
+
+------------------------------------------
+CREATE PROC usp_ThemHocVien @mshv CHAR(10),@ho nvarchar(20),@ten nvarchar(10),@ns datetime,@phai nvarchar(3),@malop CHAR(4)
+AS
+	IF EXISTS (SELECT * FROM dbo.Lop WHERE MaLop=@malop)
+		BEGIN
+		    IF EXISTS (SELECT * FROM dbo.HocVien WHERE MSHV=@mshv)
+				PRINT N'Đã tồn tại học viên trong CSDL'
+			ELSE 
+				BEGIN
+				    INSERT INTO dbo.HocVien
+				    VALUES
+				    (   @mshv,        -- MSHV - char(10)
+				        @ho,       -- Ho - nvarchar(20)
+				        @ten,       -- Ten - nvarchar(10)
+				        @ns, -- NgaySinh - datetime
+				        @phai,        -- Phai - char(3)
+				        @malop         -- MaLop - char(4)
+				        )
+						PRINT N'Đã thêm dữ liệu vào bảng thành công'
+				END
+		END
+	ELSE
+		PRINT N'Không tồn tại lớp trong CSDL'
+
+-------------------------
+set dateformat dmy
+go
+exec usp_ThemHocVien 'A07501',N'Lê Văn', N'Minh', '10/06/1988',N'Nam', 'A075'
+exec usp_ThemHocVien 'A07502',N'Nguyễn Thị', N'Mai', '20/04/1988',N'Nữ', 'A075'
+exec usp_ThemHocVien 'A07503',N'Lê Ngọc', N'Tuấn', '10/06/1984',N'Nam', 'A075'
+exec usp_ThemHocVien 'E11401',N'Vương Tuấn', N'Vũ', '25/03/1979',N'Nam', 'E114'
+exec usp_ThemHocVien 'E11402',N'Lý Ngọc', N'Hân', '01/12/1985',N'Nữ', 'E114'
+exec usp_ThemHocVien 'E11403',N'Trần Mai', N'Linh', '04/06/1980',N'Nữ', 'E114'
+exec usp_ThemHocVien 'W12301',N'Nguyễn Ngọc', N'Tuyết', '12/05/1986',N'Nữ', 'W123'
+
+
+--------------------------------------
+create PROC usp_ThemHocPhi
+@SoBL char(4),
+@MSHV char(10),
+@NgayThu Datetime,
+@SoTien	int,
+@NoiDung nvarchar(20),
+@NguoiThu nvarchar(10)
+As
+	If exists(select * from HocVien where MSHV = @MSHV) --kiểm tra có RBTV khóa ngoại
+	  Begin
+		if exists(select * from HocPhi where SoBL = @SoBL) --kiểm tra có trùng khóa(SoBL) 
+			print N'Đã có số biên lai học phí này trong CSDL!'
+		else
+		 begin
+			insert into HocPhi values(@SoBL,@MSHV,@NgayThu, @SoTien, @NoiDung,@NguoiThu)
+			print N'Thêm biên lai học phí thành công.'
+		 end
+	  End
+	Else
+		print N'Học viên '+ @MSHV + N' không tồn tại trong CSDL nên không thể thêm biên lai học phí của học viên này!'
+GO
+--------------------------
+set dateformat dmy
+go
+exec usp_ThemHocPhi '0001','E11401','16/12/2008',150000,'HP Access 2-4-6', N'Lan'
+exec usp_ThemHocPhi '0002','E11402','16/12/2008',100000,'HP Access 2-4-6', N'Lan'
+exec usp_ThemHocPhi '0003','E11403','18/12/2008',150000,'HP Access 2-4-6', N'Vân'
+exec usp_ThemHocPhi '0004','W12301','15/01/2009',50000,'HP Access 2-4-6', N'Vân'
+exec usp_ThemHocPhi '0005','A07501','02/01/2008',120000,'HP Excel 3-5-7', N'Vân'
+exec usp_ThemHocPhi '0006','A07502','02/01/2008',120000,'HP Excel 3-5-7', N'Vân'
+exec usp_ThemHocPhi '0007','A07503','02/01/2008',80000,'HP Excel 3-5-7', N'Vân'
+exec usp_ThemHocPhi '0008','A07502','18/02/2008',100000,'HP Word 2-4-6', N'Lan'
+
+--b.Cập nhật thông tin của một học viên cho trước
+CREATE PROC usp_UpdateHocVien @mshv CHAR(10),@ho nvarchar(20),@ten nvarchar(10)
+AS
+	IF EXISTS (SELECT * FROM dbo.HocVien WHERE MSHV=@mshv)
+		BEGIN
+		    UPDATE dbo.HocVien SET Ho=@ho,Ten=@ten WHERE MSHV=@mshv
+			PRINT N'Cập nhật dữ liệu của học viên '+@mshv+N' thành công'
+			SELECT * FROM dbo.HocVien WHERE MSHV=@mshv
+		END
+	ELSE 
+		PRINT N'Không tìm thấy thông tin của học viên có mã số '+@mshv+N' trong CSDL'
+
+
+---------------------------
+exec usp_UpdateHocVien 'A07501',N'Lê Văn', N'Minh'
+exec usp_UpdateHocVien 'A07502',N'Nguyễn Thị', N'Mai'
+exec usp_UpdateHocVien 'A07503',N'Lê Ngọc', N'Tuấn'
+exec usp_UpdateHocVien 'E11401',N'Vương Tuấn', N'Vũ'
+exec usp_UpdateHocVien 'E11402',N'Lý Ngọc', N'Hân'
+exec usp_UpdateHocVien 'E11403',N'Trần Mai', N'Linh'
+exec usp_UpdateHocVien 'W12301',N'Nguyễn Ngọc', N'Tuyết'
+
+
+--c.Xóa một học viên cho trước
+CREATE PROC usp_DeleteHocVien @mshv CHAR(10)
+AS
+	IF EXISTS(SELECT * FROM dbo.HocVien WHERE MSHV=@mshv)
+		BEGIN
+			DELETE FROM dbo.HocPhi WHERE MSHV=@mshv
+		    DELETE FROM dbo.HocVien WHERE MSHV=@mshv
+			PRINT N'Đã xóa thành công học viên '+@mshv+N' ra khỏi CSDL'
+		END
+	ELSE 
+		PRINT N'Không tồn tại học viên '+@mshv+N' Trong CSDL'
+	
+--------------------------
+EXEC usp_DeleteHocVien 'W12301'
+
+SELECT * FROM dbo.HocVien
+
+--d.Cập nhật thông tin của một lớp học cho trước
+CREATE PROC usp_UpdateLop @malop CHAR(4),@tenlop varchar(15),@ngaykg datetime,@hocphi int,@ca tinyint,@sotiet tinyint,@sohv tinyint,@magv CHAR(4)
+AS
+	IF EXISTS (SELECT * FROM dbo.Lop WHERE MaLop=@malop)
+		BEGIN
+		    IF EXISTS (SELECT * FROM dbo.CaHoc WHERE Ca=@ca) AND EXISTS(SELECT * FROM dbo.GiaoVien WHERE MSGV=@magv)
+				BEGIN
+				    UPDATE dbo.Lop SET TenLop=@tenlop,NgayKG=@ngaykg,HocPhi=@hocphi,Ca=@ca,SoTiet=@sotiet,SoHV=@sohv,MSGV=@magv WHERE MaLop=@malop
+					PRINT N'Đã cập nhật thành công thông tin của lớp '+@malop
+				END
+			ELSE
+				BEGIN
+				    IF NOT EXISTS(SELECT*FROM dbo.CaHoc WHERE Ca=@ca)
+						PRINT N'Không tồn tại ca học trong CSDL'
+					ELSE 
+						PRINT N'Không tồn tại giáo viên trong CSDL'
+				END
+		END
+	ELSE
+		PRINT N'Không tồn tại lớp '+@malop+N' trong CSDL'
+-------------------------
+EXEC usp_UpdateLop 'W123',N'Word 4-6-8','02/18/2008', 100000,2,30,1,'G004'
+
+--e.Xóa một lớp học cho trước nếu lớp này không có học viên
+CREATE PROC usp_DeleteLop @malop CHAR(4)
+AS
+	IF EXISTS (SELECT * FROM dbo.Lop WHERE MaLop=@malop AND SoHV =0)
+		BEGIN
+			DELETE FROM dbo.HocVien WHERE MaLop=@malop
+		    DELETE FROM dbo.Lop WHERE MaLop=@malop
+			PRINT N'Xóa thành công lớp '+@malop
+		END
+	ELSE 
+		BEGIN
+		    IF NOT EXISTS (SELECT * FROM dbo.Lop WHERE MaLop=@malop AND SoHV=0)
+				PRINT N'Không xóa được vì số học viên của lớp lớn hơn 0'
+			ELSE IF NOT EXISTS (SELECT * FROM dbo.Lop WHERE MaLop=@malop)
+				PRINT N'Không tồn tại mã lớp '+@malop+N' trong CSDL' 
+		END
+
+----------------------------------------
+EXEC usp_DeleteLop 'W124'
+---------------------------------------
+--f.Lập danh sách học viên của một lớp cho trước
+CREATE PROC usp_LapDSLop @malop CHAR(4)
+AS
+	IF EXISTS(SELECT * FROM dbo.Lop WHERE MaLop=@malop)
+		BEGIN
+		    SELECT *
+			FROM dbo.HocVien
+			WHERE MaLop = @malop
+		END
+	ELSE
+		PRINT N'Không tồn tại lớp '+@malop
+
+--------------------------
+EXEC usp_LapDSLop 'A075'
+--------------------------
+--g.Lập danh sách học viên chưa đóng đủ học phí của một lớp cho trước
+CREATE PROC usp_DSHVChuaDuHP @malop CHAR(4)
+AS 
+	IF EXISTS (SELECT * FROM dbo.Lop WHERE MaLop=@malop)
+		BEGIN
+		    
+		END
+
+		SELECT * FROM dbo.Lop
+		SELECT * FROM dbo.HocPhi
